8c8ef7b1b0f7c592fcf2527ea5a7e15a668fc263
ShadowStalfos <40166524+ShadowStalfos@users.noreply.github.com>
2024-10-24 15:26:15 +0200

    Fixed 3D Dice and included baseline enet

--- 3D_dice.py
+++ 3D_dice.py
@@ -7,15 +7,23 @@ import numpy as np
 ROOT = None
 assert ROOT is not None, "Replace the root path with the path to the project folder"
 
-models = ["deeplabv3plus", "enet", "unet", "unetpp"]
+models = ["baseline_enet", "deeplabv3plus", "enet", "unet", "unetpp"]
 
 model_result = []
 for model in models:
-    total = np.zeros(6)
+    total = np.zeros(5)
+    best = 0
+    worst = 1
     for patient in [1, 2, 13, 16, 21, 22, 28, 30, 35, 39]:
-        results = dice_metrics(f"{ROOT}data/train/Patient_{patient:02d}/GT.nii.gz", f"{ROOT}volumes/{model}/ce/Patient_{patient:02d}.nii.gz", output_path=None, indices={"Esophagus": 63, "Heart": 126, "Trachea": 189, "Aorta": 252})["Mean"]
-        total += [results["Esophagus"], results["Heart"], results["Trachea"], results["Aorta"], results["Mean"], results["Weighted mean"]]
-    model_result.append(total/10)
+        results = dice_metrics(f"{ROOT}data/segthor_train/train/Patient_{patient:02d}/GT.nii.gz", f"{ROOT}volumes/finalv2/{model}/Patient_{patient:02d}.nii.gz", output_path=None, indices={"Esophagus": 63, "Heart": 126, "Trachea": 189, "Aorta": 252})["Mean"]
+        total += [results["Esophagus"], results["Heart"], results["Trachea"], results["Aorta"], results["Mean"]]
+        if results["Mean"] > best:
+            best = results["Mean"]
+            best_patient = patient
+        if results["Mean"] < worst:
+            worst = results["Mean"]
+            worst_patient = patient
+    model_result.append([total/10, best_patient, worst_patient])
 
 for model, results in zip(models, model_result):
     print(model, results)
